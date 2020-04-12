372f702b3995e8204c86c93a1afa8f4c06143884
Thomas Geiger <tom.geiger@alp54.com>
2020-04-13 00:15:29 +0200

    unprivilidged install, have dotnet

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # basic updates/upgrades
-apt-get update
+sudo apt-get update
 #apt-get dist-upgrade -y
-apt-get install -y docker.io vim curl wget openjdk-8-jdk
+sudo apt-get install -y docker.io vim curl wget openjdk-8-jdk
 
 # have sdkman
 curl -s "https://get.sdkman.io" | bash
@@ -15,19 +15,30 @@ sdk install gradle 6.3
 sdk install java 14.0.0.j9-adpt
 sdk default java 14.0.0.j9-adpt
 
+# have ms stuff
+if [[ -x ".dotnet/dotnet" ]]
+then
+    echo "dotnet already installed, skipping"
+else
+    wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh && chmod +x dotnet-install.sh && ./dotnet-install.sh
+    echo "export PATH=$PATH:~/.dotnet" >> ~/.bashrc
+fi   
+~/.dotnet/dotnet tool install --global PowerShell
+
 # install okd cli
 if [[ -x "/usr/bin/oc" ]]
 then
     echo "oc already installed, skipping"
 else
     curl -s -S -L https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz | tar -xz
-    cp openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/oc /usr/bin/
+    sudo cp openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/oc /usr/bin/
     rm -rf openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit
 fi
 
 # install IDEs
-snap install --classic eclipse
-snap install --classic vscode
+sudo snap install --classic eclipse
+sudo snap install --classic vscode
 
 # set keyboard
 setxkbmap de
+echo "setxkbmap de" >> ~/.bashrc
